a58eef38fe91ec34337f407429c4384dcaccb2ad
JUALOBE1 <ljuan7397@gmail.com>
2021-11-19 09:51:52 +0100

    funcionamiento de cat.js

--- proyecto/myapp/wrapper/cat.js
+++ proyecto/myapp/wrapper/cat.js
@@ -27,7 +27,7 @@ const lecturaXML = (direccion) => {
 	return data;
 };
 
-let cat = lecturaXML('./static/biblioteques.xml');
+let cat = lecturaXML('../static/biblioteques.xml');
 
 let json = parser.toJson(cat);
 json = JSON.parse(json);
@@ -66,4 +66,119 @@ const creacionInsertProvincia = async (fichero) => {
 	con.end();
 };
 
-creacionInsertProvincia(json.response.row);
+const creacionInsertLocalidad = async (fichero) => {
+	let insertLocalidad = 'INSERT INTO localidad (id_provincia, nombre , codigo) VALUES ';
+	let localidades = [];
+	let provincias = await con.awaitQuery('SELECT * FROM `provincia`');
+
+	for (let i = 0; i < fichero.length; i++) {
+		let contadorLocalidades = 0;
+
+		// Buscar duplicados
+		for (let j = 0; j < localidades.length; j++) {
+			if (fichero[i].cpostal.substr(0, 2) == localidades[j].codigo) {
+				contadorLocalidades++;
+			}
+		}
+
+		// Construccion del string
+		for (let k = 0; k < provincias.length; k++) {
+			if (contadorLocalidades == 0) {
+				// creacion del objeto para evitar duplicados
+				if (fichero[i].cpostal.substr(0, 2) == provincias[k].codigo) {
+					localidades.push({
+						id_provincia: provincias[k].id_provincia,
+						nombre: fichero[i].poblacio,
+						codigo: fichero[i].codi_municipi.substr(
+							3,
+							fichero[i].codi_municipi.length
+						),
+					});
+					// Creacion del string
+					insertLocalidad += `(${
+						localidades[localidades.length - 1].id_provincia
+					}, "${localidades[localidades.length - 1].nombre}", ${
+						localidades[localidades.length - 1].codigo
+					}),`;
+					contadorLocalidades++;
+				}
+			}
+		}
+	}
+	insertLocalidad = insertLocalidad.substring(0, insertLocalidad.length - 1);
+	await con.awaitQuery(insertLocalidad);
+};
+
+const creacionInsertBiblioteca = async (fichero) => {
+	let localidades = await con.awaitQuery('SELECT * FROM `localidad`');
+	let bibliotecas = [];
+	let insertBiblioteca =
+		'INSERT INTO biblioteca (codigoPostal, descripcion, email, id_localidad, latitud, longitud, nombre, telefono, tipo, direccion) VALUES ';
+	for (let i = 0; i < fichero.length; i++) {
+		let contadorBibliotecas = 0;
+
+		// Buscar duplicados
+		for (let j = 0; j < bibliotecas.length; j++) {
+			if (fichero[i].nom == bibliotecas[j].nombre) {
+				contadorBibliotecas++;
+			}
+		}
+
+		// Construccion del string
+		for (let k = 0; k < localidades.length; k++) {
+			if (contadorBibliotecas == 0) {
+				// creacion del objeto para evitar duplicados
+				if (fichero[i].poblacio == localidades[k].nombre) {
+					bibliotecas.push({
+						id_localidad: localidades[k].id_localidad,
+						codigoPostal: fichero[i].cpostal,
+						descripcion: fichero[i].nom.replace('"', '').replace('"', ''),
+						email: fichero[i].email,
+						latitud: fichero[i].latitud,
+						tipo:
+							typeof fichero[i].propietats !== 'undefined'
+								? fichero[i].propietats
+										.toString()
+										.indexOf('Privada') !== -1
+									? 'Privada'
+									: 'Publica'
+								: 'Publica',
+						tipo2: fichero[i].propietats,
+						longitud: fichero[i].longitud,
+						telefono:
+							typeof fichero[i].telefon1 !== 'undefined'
+								? fichero[i].telefon1
+										.replace(/ /g, '')
+										.replace('/', '')
+										.substring(0, 9)
+								: '0',
+						nombre: fichero[i].nom.replace('"', '').replace('"', ''),
+						direccion: fichero[i].via,
+					});
+
+					// Creacion del string
+					insertBiblioteca += `(${
+						bibliotecas[bibliotecas.length - 1].codigoPostal
+					}, "${bibliotecas[bibliotecas.length - 1].descripcion}", "${
+						bibliotecas[bibliotecas.length - 1].email
+					}", ${bibliotecas[bibliotecas.length - 1].id_localidad}, ${
+						bibliotecas[bibliotecas.length - 1].latitud
+					}, ${bibliotecas[bibliotecas.length - 1].longitud}, "${
+						bibliotecas[bibliotecas.length - 1].nombre
+					}", ${bibliotecas[bibliotecas.length - 1].telefono}, "${
+						bibliotecas[bibliotecas.length - 1].tipo
+					}", "${bibliotecas[bibliotecas.length - 1].direccion}"),`;
+
+					contadorBibliotecas++;
+				}
+			}
+		}
+	}
+
+	insertBiblioteca = insertBiblioteca.substring(0, insertBiblioteca.length - 1);
+	console.log(insertBiblioteca);
+	await con.awaitQuery(insertBiblioteca);
+};
+creacionInsertBiblioteca(json.response.row);
+// creacionInsertLocalidad(json.response.row);
+// creacionInsertProvincia(json.response.row);
--- proyecto/myapp/wrapper/eus.js
+++ proyecto/myapp/wrapper/eus.js
@@ -8,7 +8,7 @@ const mysql = require('mysql-await');
 let con = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: '',
+	password: 'root',
 	database: 'IEI',
 });
 con.connect(function (err) {
