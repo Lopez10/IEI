6b81436bf91323e6b90f823efdbdc347903676a3
Rossalyna_Jie <53874270+Rossalyna@users.noreply.github.com>
2022-01-24 11:34:48 +0100

    Mapa funcionando

--- proyecto/frontend/biblioteca/src/app/tab1/tab1.page.ts
+++ proyecto/frontend/biblioteca/src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BusquedaService } from '../services/busqueda.service';
 import * as Mapboxgl from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
   todo: any = {
     localidad: '',
     cp: '',
@@ -40,25 +40,52 @@ export class Tab1Page {
       ruta += `&tipo=${this.todo.tipo}`;
     }
 
+    console.log(ruta.replace('&', ''));
     this.busquedaService
       .realizarBusqueda(ruta.replace('&', ''))
       .subscribe((data) => {
         console.log(data);
         this.result = data;
+        this.cargarMarcadores(data); //pinta los marcadores
       });
   }
 
   //Mapa
+  mapa: Mapboxgl.Map;
+  marcadoresEnMapa: Array<Mapboxgl.Marker> = [];
+
   ngOnInit() {
     (Mapboxgl as any).accessToken = environment.mapboxKey;
 
-    var map = new Mapboxgl.Map({
+    this.mapa = new Mapboxgl.Map({
       container: 'mapa-mapbox', // container ID
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
       center: [-3.70256, 40.4165], // starting position [lng, lat]
       zoom: 4 // starting zoom
     })
+
+    this.mapa.dragRotate.disable();
   }
 
-  
+  cargarMarcadores(bibliotecas:any) {
+    if (this.marcadoresEnMapa !== null) {
+      for (var i = this.marcadoresEnMapa.length - 1; i >= 0; i--) {
+        this.marcadoresEnMapa[i].remove(); //elimina marcadores pintados una a una (no he encontrado forma de eliminarlos todos a la vez)
+      }
+      this.marcadoresEnMapa = []; //vacia array de marcadores
+    }
+
+    for (var library of bibliotecas) {
+      var marker = new Mapboxgl.Marker()
+        .setLngLat([library.longitud, library.latitud])
+        .setPopup(
+            new Mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML(
+                `<p>${library.nombre}</p>`
+              )
+          )
+        .addTo(this.mapa);
+      this.marcadoresEnMapa.push(marker);
+    }
+  } 
 }
